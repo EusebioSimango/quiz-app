918ff992f65feb568d344e23ec6d90063906383d
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2022-04-24 23:57:16 +0200

    Update Start.tsx

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -1,33 +1,34 @@
 import { useState, useEffect } from 'react'
 
 const Start: React.FC = () => {
-  const [questions, setQuestions] = useState(null)
+  const [questions, setQuestions] = useState<any>(null)
 
 
-	useEffect(() => {
-    fetch('https://quiz-app-api-three.vercel.app/questions/all')
+  useEffect(() => {
+    // fetch('https://quiz-app-api-three.vercel.app/questions/all')
+    fetch('http://localhost:8080/questions/all')
       .then(response => response.json())
       .then(async json => {
         const data = await json
         await setQuestions(data)
-       })
+      })
   }, [])
 
   useEffect(() => {
     if (questions) setQuestion(questions[0])
   }, [questions])
 
-  const [question, setQuestion] =  useState(null) 
-  
+  const [question, setQuestion] = useState<any>(null)
+
 
   const generateId = () => {
     const id: any = Math.floor(Math.random() * 50)
     console.log('gened:', id)
     verifyQuestion(id)
-  }  
+  }
 
-  const renderQuestion = (id: any )=> {
-    console.log('rendering:', id); 
+  const renderQuestion = (id: any) => {
+    console.log('rendering:', id);
     return setQuestion(questions[id])
   }
   const verifyQuestion = (id: any) => {
@@ -37,6 +38,7 @@ const Start: React.FC = () => {
 
   }
 
+
   const verifyAnswer = (id: any, question: any) => {
     if (id === question.rightAnswer) {
       console.log('certa a resposta')
@@ -45,21 +47,40 @@ const Start: React.FC = () => {
     return console.log('OH OH OH!', id)
   }
 
-	return (
-		<>
-			{ question && (
-        <div className="questionContainer"> 
+  const handleClick = (event: React.MouseEvent<HTMLLIElement>): void => {
+    const id: any = ((event.currentTarget).id)
+    verifyAnswer(id, question)
+  }
+
+
+  // function handleClick (event:  React.MouseEvent<HTMLLIElement, MouseEvent>){
+  //   // const id: any = ((this).id)
+  //   console.log(event.currentTarget.id)
+  //   // verifyAnswer(id, question)
+  // }
+
+
+
+  return (
+    <>
+      {question && (
+        <div className="questionContainer">
           <p className="question">{question.question}</p>
           <ul className="options">
-            <li className="option" id="a" onClick={(e) => verifyAnswer(e.target.id, question)}>{question.a}</li>
-            <li className="option" id="b" onClick={(e) => verifyAnswer(e.target.id, question)}>{question.b}</li>
-            <li className="option" id="c" onClick={(e) => verifyAnswer(e.target.id, question)}>{question.c}</li>
-            <li className="option" id="d" onClick={(e) => verifyAnswer(e.target.id, question)}>{question.d}</li>
+            <li className="option" id="a"
+              onClick={handleClick}>{question.a}</li>
+            <li className="option" id="b"
+              onClick={handleClick}>{question.b}</li>
+            <li className="option" id="c"
+              onClick={handleClick}>{question.c}</li>
+            <li className="option" id="d"
+              onClick={handleClick}>
+              {question.d}</li>
           </ul>
         </div>
       )}
-</>
-	)
+    </>
+  )
 }
 
 export default Start
